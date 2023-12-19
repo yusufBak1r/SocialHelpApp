2279e6d43a2557b93b8614386873d0dfae73342f
yusuf <yusuf.bak1r3637@gmail.com>
2023-12-19 20:37:48 +0300

    Webservice update

---
+++ SocialHelp/TableviewCell/SchalorshipGiveCell.swift
@@ -0,0 +1,23 @@
+//
+//  SchalorshipGiveCell.swift
+//  SocialHelp
+//
+//  Created by yusuf bakır on 18.12.2023.
+//
+
+import UIKit
+
+class SchalorshipGiveCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SocialHelp/ViewModel/UserDao.swift
@@ -0,0 +1,8 @@
+//
+//  UserDao.swift
+//  SocialHelp
+//
+//  Created by yusuf bakır on 18.12.2023.
+//
+
+import Foundation
